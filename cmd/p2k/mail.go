2a579d9747aeb5c1ab3273fa98007e3623db9846
lucasew <lucas59356@gmail.com>
2020-12-17 15:32:39 -0300

    try passing from (fix gmx error 550)
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- cmd/p2k/mail.go
+++ cmd/p2k/mail.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"log"
+	"net/mail"
 	"net/smtp"
 	"strings"
+
 	"github.com/scorredoira/email"
 )
 
 func SendEmail(addr ...string) error {
     log.Printf("Setting up email...")
+    server := MustGetenv("SMTP_SERVER")
+    user := MustGetenv("SMTP_USER")
+    passwd := MustGetenv("SMTP_PASSWD")
     m := email.NewMessage("", "")
+    m.From = mail.Address{Name: "p2k bot", Address: user}
     m.To = addr
     err := m.Attach("./out.mobi")
     if err != nil {
         return err
     }
-    server := MustGetenv("SMTP_SERVER")
-    user := MustGetenv("SMTP_USER")
-    auth := smtp.PlainAuth("", user, MustGetenv("SMTP_PASSWD"), strings.Split(server, ":")[0])
+    auth := smtp.PlainAuth("", user, passwd, strings.Split(server, ":")[0])
     log.Printf("Sending email...")
     return email.Send(server, auth, m)
 }
