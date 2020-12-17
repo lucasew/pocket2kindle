d6154db736ac7916089703cbcdd4f45aa275baeb
lucasew <lucas59356@gmail.com>
2020-12-17 10:08:00 -0300

    fix email n chega no kindle
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-shiori/go-readability v0.0.0-20201011032228-bdc871772408
 	github.com/google/uuid v1.1.2
 	github.com/motemen/go-pocket v0.0.0-20201204003030-43b897100651
+	github.com/scorredoira/email v0.0.0-20191107070024-dc7b732c55da
 	github.com/sendgrid/rest v2.6.2+incompatible // indirect
 	github.com/sendgrid/sendgrid-go v3.7.2+incompatible
 	golang.org/x/net v0.0.0-20201010224723-4f7140c49acb
--- mail.go
+++ mail.go
@@ -3,31 +3,21 @@ package main
 import (
 	"log"
 	"net/smtp"
-	"os"
 	"strings"
-
-	"github.com/domodwyer/mailyak"
+	"github.com/scorredoira/email"
 )
 
 func SendEmail(addr ...string) error {
     log.Printf("Setting up email...")
-    mobi, err := os.Open("./out.mobi")
+    m := email.NewMessage("", "")
+    m.To = addr
+    err := m.Attach("./out.mobi")
     if err != nil {
         return err
     }
-    defer mobi.Close()
     server := MustGetenv("SMTP_SERVER")
     user := MustGetenv("SMTP_USER")
     auth := smtp.PlainAuth("", user, MustGetenv("SMTP_PASSWD"), strings.Split(server, ":")[0])
-    mail := mailyak.New(server, auth)
-    mail.To(addr...)
-    mail.From(user)
-    mail.Subject("")
-    mail.AttachInlineWithMimeType("ebook.mobi", mobi, "application/x-mobipocket-ebook")
     log.Printf("Sending email...")
-    err = mail.Send()
-    if err != nil {
-        return err
-    }
-    return nil
+    return email.Send(server, auth, m)
 }
