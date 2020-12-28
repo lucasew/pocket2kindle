ceb94280a492f8d74c317e04b11ef91579799f3d
lucasew <lucas59356@gmail.com>
2020-12-28 08:51:45 -0300

    default.nix: add calibre as dependency
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- default.nix
+++ default.nix
@@ -6,6 +6,9 @@ pkgs.buildGoModule rec {
   version = "0.0.1";
   vendorSha256 = "0z5flbrii5dqylv5pap3svbm652jgqdsmnlzfa8nx86h3drk2fjn";
   src = ./.;
+  buildInputs = with pkgs; [
+    calibre
+  ];
   meta = with pkgs.lib; {
     description = "Pocket2Kindle: Fetch articles from pocket and send as ebook to kindle";
     homepage = "https://github.com/lucasew/pocket2kindle";
