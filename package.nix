112662daf13ddfc6a3d1b61ade1f0e4adfdcb214
lucasew <lucas59356@gmail.com>
2024-04-04 13:27:11 -0300

    nix: fix hash
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ package.nix
@@ -0,0 +1,23 @@
+{ buildGoModule
+, calibre
+, lib
+}:
+
+buildGoModule {
+  name = "p2k";
+  version = "0.0.1";
+
+  src = ./.;
+
+  vendorHash = "sha256-VjoxcxvQoG6Rcp/aqht+UhRT19bjqls29biVGPOirnw=";
+
+  buildInputs = [
+    calibre
+  ];
+
+  meta = with lib; {
+    description = "Pocket2Kindle: Fetch articles from pocket and send as ebook to kindle";
+    homepage = "https://github.com/lucasew/pocket2kindle";
+    platforms = platforms.linux;
+  };
+}
